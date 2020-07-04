4ede0a3faa8c6fdc8dcdc701df866731c94d22a8
tgreyuk <tgreyuk@gmail.com>
2020-07-04 23:19:20 +0100

    Setup initial routes

---
+++ packages/frontend/src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Entry } from 'src/app/core/data.service.types';
+
+import { DataService } from '../../core/data.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent implements OnInit {
+  entry: Entry;
+
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.dataService.getEntry(params.id).subscribe((entry) => {
+        this.entry = entry;
+      });
+    });
+  }
+}
